526a467f0e1944109d97df3a6e208115e288d172
ViniciusLeonadovky <vile.pb@gmail.com>
2020-10-30 19:47:39 -0300

    Swagger implementation

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 namespace application
 {
@@ -30,7 +31,20 @@ namespace application
       ConfigureRepository.ConfigureDependenciesRepository(services);
 
       services.AddControllers();
-
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Version = "v1",
+          Title = "crud api with dotnetcore3.1",
+          Description = "Arquitetura DDD",
+          Contact = new OpenApiContact
+          {
+            Name = "Vinicius Henrique",
+            Email = "vile.pb@gmail.com"
+          }
+        });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +54,13 @@ namespace application
       {
         app.UseDeveloperExceptionPage();
       }
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "crud api with dotnetcore3.1");
+        c.RoutePrefix = string.Empty;
+      });
 
       app.UseRouting();
 
